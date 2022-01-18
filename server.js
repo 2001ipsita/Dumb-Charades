100ac5b981071717594e4023659b35c89afbc163
2001ipsita <97302947+2001ipsita@users.noreply.github.com>
2022-01-18 12:32:38 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,58 +1,37 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
-const {UserJoin, getCurrentUser,userLeave,getRoomUsers} = require('./utils/users');
-
+const express = require("express");
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-const botname = 'ChatCord Bot';
-//Run when a client connects
-io.on('connection', socket => {
-    socket.on('joinRoom', ({username,room}) => {
-        const user = UserJoin(socket.id,username,room);
-        socket.join(user.room);
-    //Welcome current user
-    // socket.emit('message', formatMessage(botname,'Welcome to chat cord'));
-
-    //Broadcast when a user connects
-    // socket.broadcast.to(user.room).emit('message', formatMessage(botname,`${user.username} joined`));
-        //send users and room info
-        io.to(user.room).emit('roomusers',{
-        room: user.room,
-        users: getRoomUsers(user.room)
-        });
-    });
-
-    
-
-    //Runs when client dissconnects
-    socket.on('disconnect', () => {
-        const user = userLeave(socket.id);
-        if(user){
-            // io.to(user.room).emit('message', formatMessage(botname,`${user.username} has left the chat`));
-            //send users and room info
-            io.to(user.room).emit('roomusers',{
-            room: user.room,
-            users: getRoomUsers(user.room)
-            });
-        }
-        
-    });
-
-    //Listen for chatMessage
-    // socket.on('chatMessage', msg => {
-    //     const user = getCurrentUser(socket.id);
-
-    //     io.to(user.room).emit('message', formatMessage(user.username,msg));
-    // });
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
 });
 
-const PORT = process.env.PORT || 8080;
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3030);
